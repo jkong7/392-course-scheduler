aa2a65c9a04c158d0a591040ba2eec37e10f69ee
Jonathan Kong <jonathankong@jonathansmbp67.ads.northwestern.edu>
2024-10-03 14:45:18 -0500

    Styled cards

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,15 +1,20 @@
 import React from 'react'; 
+import '../styles/courseList.css';
+
 
 const CourseList = ({ courses }) => (
-    <ul>
-        { Object.entries(courses).map(([id, course]) => (
-            <li key={id}>
-                <strong>{course.term} {course.number}</strong>: {course.title} <br/> 
-                Meets: {course.meets}
-            </li>
-        ))}
-    </ul>
-); 
+    <div className="course-grid">
+      {Object.entries(courses).map(([id, course]) => (
+        <div key={id} className="course-card">
+          <strong className='course-name'>{course.term} CS {course.number}</strong>
+          <p className='course-title'>{course.title}</p>
+          <p className='course-meets'>{course.meets}</p>
+        </div>
+      ))}
+    </div>
+  );
+  
+  
   
 export default CourseList;
   
